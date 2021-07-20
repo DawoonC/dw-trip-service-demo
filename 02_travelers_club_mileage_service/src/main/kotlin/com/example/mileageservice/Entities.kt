30572234a207b3c8ebec6e7d9b94dc69aa3499d7
Dawoon Choi <saffron31313131@gmail.com>
2021-07-20 19:53:35 +0900

    modified column definition of entities

--- 02_travelers_club_mileage_service/src/main/kotlin/com/example/mileageservice/Entities.kt
+++ 02_travelers_club_mileage_service/src/main/kotlin/com/example/mileageservice/Entities.kt
@@ -1,8 +1,10 @@
 package com.example.mileageservice
 
+import org.hibernate.annotations.GenericGenerator
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
+import java.util.*
 import javax.persistence.*
 
 @Entity
@@ -18,10 +20,10 @@ class Review(
   @Column(name="place_id", length=255, nullable=false) var placeId: String,
   @Lob @Column(name="content", nullable=false) var content: String = "",
   @Lob @Column(name="photo_ids", nullable=false) var photoIds: String = "",
+  @Column(name="has_first_review_point", length=1, nullable=false) var hasFirstReviewPoint: Boolean = false,
   @CreatedDate @Column(name="created_at", nullable=false) var createdAt: LocalDateTime = LocalDateTime.now(),
   @LastModifiedDate @Column(name="modified_at", nullable=false) var modifiedAt: LocalDateTime = LocalDateTime.now(),
-  @Column(name="is_deleted", length=1, nullable=false) var isActive: Boolean = false,
-  @Id @GeneratedValue(strategy=GenerationType.AUTO) var id: String? = null,
+  @Id var id: String = UUID.randomUUID().toString(),
 )
 
 @Entity
@@ -34,7 +36,7 @@ class UserPoint(
   @Column(name="point", nullable=false) var point: Int = 0,
   @CreatedDate @Column(name="created_at", nullable=false) var createdAt: LocalDateTime = LocalDateTime.now(),
   @LastModifiedDate @Column(name="modified_at", nullable=false) var modifiedAt: LocalDateTime = LocalDateTime.now(),
-  @Id @GeneratedValue(strategy=GenerationType.AUTO) var id: String? = null,
+  @Id var id: String = UUID.randomUUID().toString(),
 )
 
 @Entity
@@ -47,5 +49,5 @@ class UserPointHistory(
   @CreatedDate @Column(name="created_at", nullable=false) var createdAt: LocalDateTime = LocalDateTime.MIN,
   @Column(name="increased_amount", nullable=false) var increasedAmount: Int = 0,
   @Column(name="decreased_amount", nullable=false) var decreasedAmount: Int = 0,
-  @Id @GeneratedValue(strategy=GenerationType.AUTO) var id: String? = null,
+  @Id var id: String = UUID.randomUUID().toString(),
 )
